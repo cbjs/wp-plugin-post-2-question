18f335a9a69034a435bf226943c6fdb4ffcb4c8a
cbjs <cbjs.deng@gmail.com>
2013-12-05 16:10:04 +0800

    first commit

---
+++ post-2-question.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: Post to Question
+ * Plugin URI: http://codetodeath.com/wp/plugin/post_2_question
+ * Description: A wordpress plugin to add an question on question2answer site while posting.
+ * Version: 1.0
+ * Author: Deng Guo
+ * Author URI: http://codetodeath.com
+ * License: GPL2
+ */
+
+define(QA_BASE_DIR, 'c:/wamp/apps/discuss/');
+
+require_once QA_BASE_DIR . 'qa-include/qa-base.php';
+
+require_once QA_INCLUDE_DIR.'qa-app-users.php';
+require_once QA_INCLUDE_DIR.'qa-app-posts.php';
+	
+function post2question($postId) {
+    $post = get_post($postId);
+    $tags = wp_get_post_tags($postId, array('fields' => 'names'));
+
+    if ($post) {
+        qa_post_create(
+            'Q',                    // type question
+            null,                   // no parentid
+            $post->post_title,      // question title
+            $post->post_excerpt,    // question content
+            '',                     // format: plain text
+            null,                   // category id
+            $tags,                  // tags
+            1                       // user id
+        );
+    }
+}
+
+add_action('publish_post', 'post2question');

be8889f54bda4b70a4be2e346f4981fa1f5ac528
cbjs <cbjs.deng@gmail.com>
2013-12-07 21:26:10 +0800

    add content filter

---
+++ post-2-question-options.php
@@ -0,0 +1,42 @@
+<?php
+// option page ui
+// register options
+function post2question_option_init() {
+  register_setting('p2q_options', 'p2q_url_prefix');
+}
+
+add_action('admin_init', 'post2question_option_init');
+
+function post2question_option_ui() {
+  ?>
+  <div class="wrap">
+    <?php screen_icon(); ?>
+    <h2>Post to Question Settings</h2>
+    <p>Welcome to the Post2Question Plugin. Here you can edit many options used by the plugin.</p>
+    <form id="wp-plugin-post-2-question-option-form" action="options.php" method="post">
+      <?php settings_fields('p2q_options'); ?>
+      <table class="form-table">
+        <tbody>
+          <tr valign="top">
+            <th scope="row">
+              <label for="p2q_url_prefix"> Url Prefix</label>
+            </th>
+            <td>
+              <input type="text" id="p2q_url_prefix" name="p2q_url_prefix" class="regular-text code" value="<?php echo esc_attr(get_option('p2q_url_prefix'));?>" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p class="submit">
+        <input type="submit" name="submit" value="Save Changes" class="button button-primary"/>
+      </p>
+    </form>
+  </div>
+  <?php
+}
+
+function post2question_option_hook() {
+  add_options_page('Post to Question Setting', 'Post2Question', 'manage_options', 'wp-plugin-post-2-question', 'post2question_option_ui');
+}
+
+add_action('admin_menu', 'post2question_option_hook');
--- post-2-question.php
+++ post-2-question.php
@@ -12,16 +12,16 @@
 define(QA_BASE_DIR, '/Applications/MNPP/htdocs/disucss/');
 
 require_once QA_BASE_DIR . 'qa-include/qa-base.php';
-
 require_once QA_INCLUDE_DIR.'qa-app-users.php';
 require_once QA_INCLUDE_DIR.'qa-app-posts.php';
+require_once 'post-2-question-options.php';
 	
 function post2question($postId) {
     $post = get_post($postId);
     $tags = wp_get_post_tags($postId, array('fields' => 'names'));
 
     if ($post) {
-        qa_post_create(
+        $qId = qa_post_create(
             'Q',                    // type question
             null,                   // no parentid
             $post->post_title,      // question title
@@ -31,7 +31,26 @@ function post2question($postId) {
             $tags,                  // tags
             1                       // user id
         );
+
+        if ($qId) {
+          add_post_meta($postId, 'disucss_qId', $qId);
+        }
     }
 }
 
 add_action('publish_post', 'post2question');
+
+function p2q_add_discuss_btn($content) {
+  $cur_post = get_post();
+  if ( $cur_post->post_type == 'post' ) {
+    $qId = get_post_meta($cur_post->ID, 'disucss_qId', true);
+    if ( $qId ) {
+      $btn_url = get_option('p2q_url_prefix') . $qId;
+      $discuss_btn = '<a id="q2a_btn" target="_blank" href="' . $btn_url . '">disucss</a>';
+      return $discuss_btn . $content;
+    }
+  }
+  return $content;
+}
+
+add_filter('the_content', 'p2q_add_discuss_btn');
